3293994a5e4a59aa608bfd085e140fcaa6e17e70
Karan Yede <115563014+karanyede@users.noreply.github.com>
2025-07-07 20:02:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const target = document.getElementById("scrambleText");
 const button = document.getElementById("toggleBtn");
-const message = "Creativity meets code.";
+const message = "Deciphered. Click Here!";
 const scrambleChars =
   "!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 let isDecoded = false;
